41e63990a989a5d89de663556277cfe8475db5b1
Eric Bragas <eric@flywheelsoftware.com>
2020-04-05 15:08:00 -0700

    methods for returning each object type

--- parsehub/client.py
+++ parsehub/client.py
@@ -1,4 +1,7 @@
 import requests
+import json
+import logging
+from datetime import datetime
 
 # What's this client going to be able to do?
 # Get projects
@@ -20,9 +23,8 @@ class ParseHubClient(object):
         self.api_key = api_key
 
     def get_project(self, project_token, offset=0, include_options=0):
-        
+
         project_endpoint = f'https://www.parsehub.com/api/v2/projects/{project_token}'
-        
         params = {
             "api_key": self.api_key,
             "offset": offset,
@@ -32,15 +34,23 @@ class ParseHubClient(object):
         project = ParseHubProject(**response.json())
         return project 
 
-    def get_all_projects(self, offset=0, limit=2, include_options=0):
+    def list_projects(self, offset=0, limit=2, include_options=0):
+        
+        project_endpoint = 'https://www.parsehub.com/api/v2/projects'
         params = {
             'api_key': self.api_key,
             'offset': offset,
             'limit': limit,
             'include_options': include_options
         }
-        r = requests.get('https://www.parsehub.com/api/v2/projects', params=params)
-        return r
+        response = requests.get(project_endpoint, params=params)
+        
+        # return response
+        project_list = []
+        for project_json in response.json()['projects']:
+            project_list.append(ParseHubProject(**project_json))
+
+        return project_list
 
 
 class ParseHubProject(object):
@@ -53,7 +63,11 @@ class ParseHubProject(object):
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
-    self.last_ready_run = ParseHubRun(**self.last_ready_run)
+        # value formatting
+        try:
+            self.last_ready_run = ParseHubRun(**self.last_ready_run)
+        except AttributeError:
+            raise AttributeError # until I know when this happens
 
 
 class ParseHubRun(object):
@@ -62,6 +76,14 @@ class ParseHubRun(object):
         
         for dictionary in initial_data:
             for key in dictionary:
-                setattr(self, key, dictionary[key])
+                setattr(self, key, None if dictionary[key]=='' else dictionary[key])
         for key in kwargs:
-            setattr(self, key, kwargs[key])
+            setattr(self, key, None if kwargs[key]=='' else kwargs[key])
+
+        # # value formatting
+        # self.start_value = json.loads(self.start_value) if self.start_value else None
+        # self.options_json = json.loads(self.options_json) if self.options_json else None
+        # self.data_ready = bool(self.data_ready)
+        # self.start_time = datetime.fromisoformat(self.start_time)
+        # self.end_time = datetime.fromisoformat(self.end_time)
+        # self.start_running_time = datetime.fromisoformat(self.start_running_time)
