e9497da996e96e5325c0ddc9318bff3b57234ad8
Eric Bragas <eric@flywheelsoftware.com>
2020-04-05 15:57:38 -0700

    additional run methods

--- parsehub/client.py
+++ parsehub/client.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import logging
+from csv import DictReader
 from datetime import datetime
 
 # What's this client going to be able to do?
@@ -22,13 +23,22 @@ class ParseHubClient(object):
     def authenticate(self, api_key):
         self.api_key = api_key
 
+    def get_run(self, run_token):
+        '''Documentation: https://www.parsehub.com/docs/ref/api/v2/?python#get-a-run'''
+        run_endpoint = f'https://www.parsehub.com/api/v2/runs/{run_token}'
+        params = {'api_key': self.api_key}
+        
+        response = requests.get(run_endpoint, params=params)
+        run = ParseHubRun(**response.json(), client=self)
+        return run
+    
     def get_project(self, project_token, offset=0, include_options=0):
 
         project_endpoint = f'https://www.parsehub.com/api/v2/projects/{project_token}'
         params = {
-            "api_key": self.api_key,
-            "offset": offset,
-            "include_options": include_options
+            'api_key': self.api_key,
+            'offset': offset,
+            'include_options': include_options
         }
         response = requests.get(project_endpoint, params=params)
         project = ParseHubProject(**response.json())
@@ -71,9 +81,11 @@ class ParseHubProject(object):
 
 
 class ParseHubRun(object):
-    '''A signle ParseHub Run'''
-    def __init__(self, *initial_data, **kwargs):
+    '''A single ParseHub Run'''
+    def __init__(self, client, *initial_data, **kwargs):
         
+        self.client = client
+
         for dictionary in initial_data:
             for key in dictionary:
                 setattr(self, key, None if dictionary[key]=='' else dictionary[key])
@@ -87,3 +99,30 @@ class ParseHubRun(object):
         # self.start_time = datetime.fromisoformat(self.start_time)
         # self.end_time = datetime.fromisoformat(self.end_time)
         # self.start_running_time = datetime.fromisoformat(self.start_running_time)
+
+    def get_data(self, format='json'):
+        '''Documentation: https://www.parsehub.com/docs/ref/api/v2/?python#get-data-for-a-run'''
+        run_data_endpoint = f'https://www.parsehub.com/api/v2/runs/{self.run_token}/data'
+        params = {'api_key': self.client.api_key, 'format': format}
+        
+        response = requests.get(run_data_endpoint, params=params)
+        if format == 'json':
+            return response.json()
+        else:
+            data = DictReader(response.text.splitlines())
+            return list(data)
+
+    def cancel(self):
+        '''Documentation: https://www.parsehub.com/docs/ref/api/v2/?python#cancel-a-run'''
+        cancel_run_endpoint = f"https://www.parsehub.com/api/v2/runs/{self.run_token}/cancel"
+        params = {"api_key": self.client.api_key}
+        response = requests.post(cancel_run_endpoint, data=params)
+        return response.json()
+
+    def delete(self):
+        '''Documentation: https://www.parsehub.com/docs/ref/api/v2/?python#delete-a-run'''
+        delete_run_endpoint = f'https://www.parsehub.com/api/v2/runs/{self.run_token}'
+        params = {"api_key": self.client.api_key}
+        
+        response = requests.delete(delete_run_endpoint, params=params)
+        return response.json()
